1bd9b66a3e975a975ddd66950563a4396877f583
Sardor Narziyev <sardornarziyev2000@gmail.com>
2021-09-05 18:50:15 +0500

    Finished web app tutorial with ktor

--- src/main/kotlin/com/jetbrains/handson/website/Application.kt
+++ src/main/kotlin/com/jetbrains/handson/website/Application.kt
@@ -1,20 +1,66 @@
 package com.jetbrains.handson.website
 
-import freemarker.cache.*
+import freemarker.cache.ClassTemplateLoader
 import freemarker.core.HTMLOutputFormat
 import io.ktor.application.*
 import io.ktor.freemarker.*
-import io.ktor.html.respondHtml
-import io.ktor.http.HttpStatusCode
+import io.ktor.html.*
+import io.ktor.http.*
 import io.ktor.http.content.*
-import io.ktor.request.receiveParameters
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import kotlinx.html.*
 
+val blogEntries = mutableListOf(BlogEntry(
+    "The drive to develop!",
+    "...it's what keeps me going."
+))
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 fun Application.module() {
 
+    install(FreeMarker) {
+        templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
+        outputFormat = HTMLOutputFormat.INSTANCE
+    }
+
+    routing {
+        static("/static") {
+            resources("files")
+        }
+        get("/") {
+            call.respond(FreeMarkerContent("index.ftl", mapOf("entries" to blogEntries), ""))
+        }
+        post("/submit") {
+            val params = call.receiveParameters()
+            val headline = params["headline"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val body = params["body"] ?: return@post call.respond(HttpStatusCode.BadRequest)
+            val newEntry = BlogEntry(headline, body)
+            blogEntries.add(0, newEntry)
+            // TODO: send a status page to the user
+            call.respondHtml {
+                body {
+                    h1 {
+                        +"Thanks for submitting your entry!"
+                    }
+                    p {
+                        +"We've submitted your new entry titled "
+                        b {
+                            +newEntry.headline
+                        }
+                    }
+                    p {
+                        +"You have submitted a total of ${blogEntries.count()} articles!"
+                    }
+                    a("/") {
+                        +"Go back"
+                    }
+                }
+            }
+        }
+    }
 }
+
+data class BlogEntry(val headline: String, val body: String)
